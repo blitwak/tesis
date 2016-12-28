95378620807b861a72315c35e49b5fd9a7aa38de
Brian Litwak <brian.litwak@gmail.com>
2016-12-28 15:37:09 -0300

    asd

--- nuevo/flaskapp/flaskapp.py
+++ nuevo/flaskapp/flaskapp.py
@@ -4,46 +4,52 @@ import json
 import preparar as preparar
 import pickle
 import os.path
-from flask_sqlalchemy import SQLAlchemy
+#from flask.ext.pymongo import PyMongo
+#from flask_pymongo import PyMongo
+#from mongokit import Connection, Document
 
+#from flask.ext.mongoalchemy import MongoAlchemy
+from flask_mongoalchemy import MongoAlchemy
+
+#from flask_sqlalchemy import SQLAlchemy
+#from boto.dynamodb2.fields import HashKey
+#from boto.dynamodb2.table import Table
+#from flask.ext.dynamo import Dynamo
+#import boto3
+#from boto.dynamodb2.layer1 import DynamoDBConnection
+#from boto.regioninfo import RegionInfo
 app = Flask(__name__)
+puerto = 8501
 app.secret_key = "holamanolacomova"
 #app.SQLALCHEMY_DATABASE_URI = 'sqlite:///students.sqlite3'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-db = SQLAlchemy(app)
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["AWS_ACCESS_KEY_ID"] = "AKIAINSPSQ3GTANJH64A"
+app.config["AWS_SECRET_ACCESS_KEY"] = "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"
+
+#https://pythonhosted.org/Flask-MongoAlchemy/
+app.config['MONGOALCHEMY_DATABASE'] = 'test'
+db = MongoAlchemy(app)
+
+class registro(db.Document):
+	colaborador = db.StringField()
+	perfilDeTwitter = db.StringField()
+	choice = db.StringField()
+
 global asd
 asd = 0
-
-
-class registro(db.Model):
-	id = db.Column('registro_id', db.Integer, primary_key = True)
-	colaborador = db.Column(db.String(100))
-	perfilDeTwitter = db.Column(db.String(64))
-	choice = db.Column(db.String(30))
-
-	def __init__(self, colaborador,perfilDeTwitter,choice):
-		self.colaborador = colaborador
-		self.perfilDeTwitter = perfilDeTwitter
-		self.choice = choice
 
 global cantPerfilesAmostrar
 cantPerfilesAmostrar = 3
 
 def levantarPerfilesDeTwitter():
-	filename = "/var/www/twitterAmostrarCopaArgentina.p"
-#	filename = "static/bd/twitterAmostrarCopaArgentina.p"
+#	filename = "/var/www/twitterAmostrarCopaArgentina.p"
+	filename = "static/bd/twitterAmostrar.p"
 	if os.path.isfile(filename):
 		filehandler = open(filename,'rb')
 		ret = pickle.load(filehandler)
 		filehandler.close()
 		print "levantada"
 		return ret
-
-#	else:
-		#print "no entro"
-		#ret = {201032752: [u'Club Atl\xe9tico Uni\xf3n', u'clubaunion', u'Twitter oficial del Club Atl\xe9tico Uni\xf3n de Santa Fe.', u'http://pbs.twimg.com/profile_images/641646225041846274/rmJgG9lN_normal.png', u'https://pbs.twimg.com/profile_banners/201032752/1445378175'], 505347396: [u'Club Atl\xe9tico Col\xf3n', u'ColonOficial', u'Bienvenidos a la cuenta oficial en Twitter del Club Atl\xe9tico Col\xf3n', u'http://pbs.twimg.com/profile_images/806325049649229824/6NAHjAtP_normal.jpg', u'https://pbs.twimg.com/profile_banners/505347396/1481673792'], 188356649: [u'C. A. Independiente', u'Independiente', u'Cuenta Oficial del Club Atl\xe9tico Independiente.', u'http://pbs.twimg.com/profile_images/803916220932247552/ugJbk-Fi_normal.jpg', u'https://pbs.twimg.com/profile_banners/188356649/1482029837'], 338400297: [u'Rosario Central', u'CARCoficial', u'Cuenta oficial del Club Atl\xe9tico Rosario Central', u'http://pbs.twimg.com/profile_images/677256336061669376/WqxaSE_X_normal.jpg', u'https://pbs.twimg.com/profile_banners/338400297/1481929521'], 276223178: [u'Club A. Temperley', u'TemperleyOK', u'\u2022 CUENTA OFICIAL DEL CLUB ATL\xc9TICO TEMPERLEY \u2022 Donde los sue\xf1os se cumplen \u2022 \u26051974 \u26051982 \u27301995 \u27301996 \u27301999 \u27302014 \u26052014 \u2022 Contacto: prensa@temperley.org.ar', u'http://pbs.twimg.com/profile_images/611209880591069184/Ly5JlfvD_normal.png', u'https://pbs.twimg.com/profile_banners/276223178/1478713188'], 783689678: [u'Club Atl\xe9tico Lan\xfas', u'clublanus', u'Twitter oficial del Club Atl\xe9tico Lan\xfas. Fundado el 3 de enero de 1915. El Club de barrio m\xe1s grande del Mundo.', u'http://pbs.twimg.com/profile_images/792875030224961537/Rq9Bx24e_normal.jpg', u'https://pbs.twimg.com/profile_banners/783689678/1475782945'], 575667065: [u"Newell's Old Boys", u'CANOBoficial', u"Club Atl\xe9tico Newell's Old Boys", u'http://pbs.twimg.com/profile_images/811620180472266752/a6g1P93J_normal.jpg', u'https://pbs.twimg.com/profile_banners/575667065/1473185077'], 162918448: [u'Atl\xe9tico Tucum\xe1n Of.', u'ATOficial', u'Twitter Oficial del Club Atl\xe9tico Tucum\xe1n. Facebook: https://t.co/pLs844M4FP\nInstagram: Clubatleticotucumanoficial', u'http://pbs.twimg.com/profile_images/803919676711571456/gCQheT6N_normal.jpg', u'https://pbs.twimg.com/profile_banners/162918448/1478312562'], 273667377: [u'Club Atl\xe9tico Tigre', u'catigreoficial', u'Twitter Oficial del Club Atl\xe9tico Tigre. Fundado el 3 de Agosto de 1902. Primera Divisi\xf3n del F\xfatbol Argentino.', u'http://pbs.twimg.com/profile_images/771342186940076032/aU2Jm0p7_normal.jpg', u'https://pbs.twimg.com/profile_banners/273667377/1472060316'], 42933426: [u'Boca Jrs. Oficial', u'BocaJrsOficial', u'Twitter oficial del Club Atl\xe9tico Boca Juniors / https://t.co/IZiKeMQJuw / https://t.co/9jE45F0GSu / https://t.co/uhVPA9vSUU.', u'http://pbs.twimg.com/profile_images/803613906329604096/uBYvVHsD_normal.jpg', u'https://pbs.twimg.com/profile_banners/42933426/1481916263'], 240008403: [u'San Lorenzo', u'SanLorenzo', u'Twitter Oficial del Club Atl\xe9tico San Lorenzo de Almagro. Informaci\xf3n actualizada, coberturas en vivo y todo acerca de la vida de San Lorenzo.', u'http://pbs.twimg.com/profile_images/805759246256242691/cFiazeMD_normal.jpg', u'https://pbs.twimg.com/profile_banners/240008403/1458772608'], 118056286: [u'GIMNASIA', u'gimnasiaoficial', u'Club de Gimnasia y Esgrima La Plata. Fundado el 3 de junio de 1887. Decano del f\xfatbol de Am\xe9rica. Campe\xf3n Argentino 1929 y Copa Centenario AFA 1993/1994.', u'http://pbs.twimg.com/profile_images/583323196243517440/sQGOuGHb_normal.jpg', u'https://pbs.twimg.com/profile_banners/118056286/1482795385'], 276204298: [u'V\xe9lez Sarsfield', u'Velez', u'Twitter Oficial del Club Atl\xe9tico V\xe9lez Sarsfield.\r\nEl primero en ser un gran club.', u'http://pbs.twimg.com/profile_images/770698276781826048/JSk38W7i_normal.jpg', u'https://pbs.twimg.com/profile_banners/276204298/1482343833'], 1112088823: [u'Belgrano', u'Belgrano', u'Twitter Oficial del Club Atl\xe9tico Belgrano.\r\nAsociaci\xf3n Civil fundada el 19 de marzo de 1905.\r\nAlberdi, C\xf3rdoba, Argentina.', u'http://pbs.twimg.com/profile_images/813387900293414912/1glAgGmm_normal.jpg', u'https://pbs.twimg.com/profile_banners/1112088823/1481728541'], 284128412: [u'Estudiantes de L.P.', u'EdelpOficial', u'Cuenta Oficial del Club Estudiantes de La Plata. #EDLP', u'http://pbs.twimg.com/profile_images/768526728385925122/HRwpUSHN_normal.jpg', u'https://pbs.twimg.com/profile_banners/284128412/1482190103'], 196845049: [u'Talleres', u'CATalleresdecba', u'Twitter Oficial del Club Atl\xe9tico Talleres de C\xf3rdoba.', u'http://pbs.twimg.com/profile_images/806860257037271040/deLsM-m0_normal.jpg', u'https://pbs.twimg.com/profile_banners/196845049/1479307122'], 274140988: [u'CA Hurac\xe1n', u'CAHuracan', u'Twitter Oficial del Club Atl\xe9tico Hurac\xe1n', u'http://pbs.twimg.com/profile_images/813114473875730432/3IpyIRRK_normal.jpg', u'https://pbs.twimg.com/profile_banners/274140988/1408402045'], 95227165: [u'Racing Club', u'RacingClub', u'Twitter oficial de Racing Club, el primer grande. Avellaneda, Argentina.', u'http://pbs.twimg.com/profile_images/806590307794096129/XtRJq-q8_normal.jpg', u'https://pbs.twimg.com/profile_banners/95227165/1479258323'], 706027358: [u'Club A. Banfield', u'CAB_oficial', u'Cuenta Oficial de Twitter del Club Atl\xe9tico Banfield. Fundado el 21 de enero de 1896.', u'http://pbs.twimg.com/profile_images/804098374593105926/ZoDoLgws_normal.jpg', u'https://pbs.twimg.com/profile_banners/706027358/1474908338'], 161734463: [u'River Plate', u'CARPoficial', u'Cuenta oficial del Club Atl\xe9tico River Plate.', u'http://pbs.twimg.com/profile_images/803808585994108928/B3TzNLVW_normal.jpg', u'https://pbs.twimg.com/profile_banners/161734463/1480526867']}
-		#return ret
-
 
 global perfilesDeTwitter
 perfilesDeTwitter = levantarPerfilesDeTwitter()
@@ -166,9 +172,8 @@ def yajugue():
 	print idTwitterCuenta
 	dicidTwitterCuentaToVotacion[idTwitterCuenta] = dicidTwitterCuentaToVotacion[idTwitterCuenta].append([name,seleccionEquipo])
 	print "antes del commit"
-	elemento = registro(name,idTwitterCuenta,seleccionEquipo)
-	db.session.add(elemento)
-	db.session.commit()
+	agregar = registro(colaborador= name ,perfilDeTwitter= idTwitterCuenta,choice=seleccionEquipo)
+	agregar.save()
 	print"coommit"
 
 	perfiles = dicEsclavoToPerfilesAver[name]    
@@ -184,8 +189,9 @@ def yajugue():
 
 @app.route('/1')
 def hello_world():
-  return 'Hello from Flask!'
+	return 'Hello from Flask!'
 
 if __name__ == '__main__':
-  db.create_all()
-  app.run()
+#  db.create_all()
+#  	users = Table.create('elementos', schema=[HashKey('username')]);
+	app.run(port=puerto)
