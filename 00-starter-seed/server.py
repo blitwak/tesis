fb6cc06cacc4b3e1b563d12c5cf75b18551eeb6a
Brian Litwak <brian.litwak@gmail.com>
2017-01-11 01:24:20 -0300

    en la mitad de la noche

--- 00-starter-seed/server.py
+++ 00-starter-seed/server.py
@@ -12,6 +12,7 @@ import requests
 from flask import Flask, request, jsonify, session, redirect, render_template, send_from_directory,url_for, escape
 from flask_mongoalchemy import MongoAlchemy
 import constants
+import codecs
 
 #https://pythonhosted.
 
@@ -37,7 +38,7 @@ app.debug = True
 
 class registro(db.Document):
 	colaborador = db.StringField()
-	perfilDeTwitter = db.StringField()
+	perfilDeTwitter = db.IntField()
 	choice = db.StringField()
 
 
@@ -71,6 +72,9 @@ dicEsclavoToPerfilesTwitterVistos = {} #user->idTwitterCuenta
 global dicEsclavoToPerfilesAver
 dicEsclavoToPerfilesAver = {}
 
+global dicEsclavoToUltimoVisto
+dicEsclavoToUltimoVisto = {}
+
 global dicUserToTuplasSeleccionadas
 dicUserToTuplasSeleccionadas = {} # user -> idTwitterCuenta,choice
 global dicidTwitterCuentaToVotacion
@@ -92,17 +96,22 @@ def requires_auth(f):
 @app.route('/')
 @app.route('/login', methods=['POST','GET'])
 def login():
-    print "aca"
     return render_template('login.html', env=env)
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return render_template('login.html', env=env)
+
 
 @app.route('/public/<path:filename>')
 def static_files(filename):
     return send_from_directory('./public', filename)
 
-@app.route('/index')
+@app.route('/index', methods=['POST','GET'])
 @requires_auth
 def index():
-    
+    print "paso por aca si iii"
     return render_template('index.html',
                            user=session[constants.PROFILE_KEY], env=env)
 
@@ -111,6 +120,148 @@ def index():
 #@requires_auth
 #def dashboard():
 #    return render_template('dashboard.html', user=session[constants.PROFILE_KEY])
+
+
+
+@app.route('/jugarPrimeraVez', methods=['POST'])
+def jugarPrimeraVez():
+    global dicEsclavoToPerfilesAver
+    global dicEsclavoToPerfilesTwitterVistos
+    global asd
+    global dicUserToTuplasSeleccionadas
+
+    name = session[constants.PROFILE_KEY]['nickname']
+    perfiles = preparar.obtenerPerfiles(name,dicEsclavoToPerfilesTwitterVistos,perfilesDeTwitter,dicPerfilesDeTwitterToCantidadVistos,cantPerfilesAmostrar) #perfilesDeTwitter[0:2]    #MIRAR ESTOOOOOOOOOOOOO SELECCION DE PERFILES
+    perfilAenviar = perfiles[0]
+    dicEsclavoToPerfilesAver[name]=perfiles[1:]
+    print name
+    msg, twitterPerf = preparar.armarMensaje(perfilAenviar,perfilesDeTwitter)
+    if name not in dicEsclavoToPerfilesTwitterVistos.keys():
+        dicEsclavoToPerfilesTwitterVistos[name]= []
+        print "grabo"
+    else:
+        print "no grabo"
+    if name not in dicUserToTuplasSeleccionadas.keys():
+        dicUserToTuplasSeleccionadas[name]= []
+
+    global dicEsclavoToUltimoVisto
+    dicEsclavoToUltimoVisto[name] = perfilAenviar
+    return render_template('jugar.html',env=env, perfiles = msg, nombreDelUsuario = twitterPerf)
+
+
+@app.route('/yajugue', methods=['POST'])
+def yajugue():
+    global asd
+    global dicEsclavoToPerfilesTwitterVistos
+    global dicUserToTuplasSeleccionadas
+    global dicidTwitterCuentaToVotacion
+    global dicEsclavoToPerfilesAver
+    global dicEsclavoToUltimoVisto
+
+
+    formulario = request.form
+    if("btn1" in formulario):
+        btn=request.form['btn1']
+        print btn
+        if(btn== 'No se'):
+            print "No se"
+            equipoSeleccionado = "No se"
+        elif(btn == "Ninguno"):
+            print "Ninguno"
+            equipoSeleccionado = "Ninguno"
+        elif(btn == "Boca"):
+            print "Boca"
+            equipoSeleccionado = "Boca"
+        elif(btn == "River"):
+            print "River"
+            equipoSeleccionado = "River"
+        elif(btn == "Racing"):
+            print "Racing"
+            equipoSeleccionado = "Racing"
+        elif(btn == "Independiente"):
+            equipoSeleccionado = "Independiente"
+            print "Independiente"
+        elif(btn == "San Lorenzo"):
+            equipoSeleccionado = "San Lorenzo"
+            print "San Lorenzo"
+        elif(btn == "Huracan"):
+            equipoSeleccionado = "Huracan"
+            print "Huracan"
+        elif(btn == "Belgrano"):
+            equipoSeleccionado = "Belgrano"
+            print "Belgrano"
+        elif(btn == "Talleres"):
+            equipoSeleccionado = "Talleres"
+            print "Talleres"
+        elif(btn == "Rosario central"):
+            equipoSeleccionado = "Rosario central"
+            print "Rosario central"
+        elif(btn == "Newell's"):
+            equipoSeleccionado = "Newell's"
+            print "Newell's"
+        elif(btn == "Estudiantes de la plata"):
+            equipoSeleccionado = "Estudiantes de la plata"
+            print "Estudiantes de la plata"
+        elif(btn == "Gimnasia"):
+            equipoSeleccionado = "Gimnasia"
+            print "Gimnasia"
+        elif(btn == "Colon"):
+            equipoSeleccionado = "Colon"
+            print "Colon"
+        elif(btn == "Velez"):
+            equipoSeleccionado = "Velez"
+            print "Velez"
+        elif(btn == "Lanus"):
+            equipoSeleccionado = "Lanus"
+            print "Lanus"
+        elif(btn == "Banfield"):
+            equipoSeleccionado = "Banfield"
+            print "Banfield"
+        elif(btn == "Atletico Tucuman"):
+            equipoSeleccionado = "Atletico Tucuman"
+            print "Atlético Tucumán"
+        elif(btn == "Temperley"):
+            equipoSeleccionado = "Temperley"
+            print "Temperley"
+        elif(btn == "Union"):
+            equipoSeleccionado = "Union"
+            print "Union"
+        elif(btn == "Tigre"):
+            equipoSeleccionado = "Tigre"
+            print "Tigre"
+        else:
+            print "boom"
+    else:
+        print "No"
+    name = session['username']
+    perfilVisto = dicEsclavoToUltimoVisto[name]
+    print name
+    print perfilVisto
+
+    dicEsclavoToPerfilesTwitterVistos[name].append(perfilVisto)
+    dicUserToTuplasSeleccionadas[name].append([perfilVisto,equipoSeleccionado])
+
+    if (perfilVisto not in dicidTwitterCuentaToVotacion.keys()):
+        dicidTwitterCuentaToVotacion[perfilVisto] = []
+        print "agregado"
+
+    elementito = [name,equipoSeleccionado]
+    dicidTwitterCuentaToVotacion[perfilVisto].append(elementito)
+
+    agregar = registro(colaborador= name ,perfilDeTwitter= perfilVisto,choice=equipoSeleccionado)
+    agregar.save()
+
+    perfiles = dicEsclavoToPerfilesAver[name]
+
+    if(len(perfiles) > 0):
+        perfilAenviar = perfiles[0]
+        dicEsclavoToPerfilesAver[name]=perfiles[1:]
+        msg, twitterPerf = preparar.armarMensaje(perfilAenviar,perfilesDeTwitter)
+        dicEsclavoToUltimoVisto[name] = perfilAenviar
+        return render_template('jugar.html',env=env, perfiles = msg, nombreDelUsuario = twitterPerf)
+    else:
+        return render_template('finJuego.html', user=name, votacion = dicUserToTuplasSeleccionadas[name])
+
 
 @app.route('/callback')
 def callback_handling():
@@ -135,6 +286,7 @@ def callback_handling():
 
     user_info = requests.get(user_url).json()
     session[constants.PROFILE_KEY] = user_info
+    session['username'] = user_info['nickname']
     return redirect('/index')
 
 if __name__ == "__main__":
